d169665dbc1fa4289267fcade92216ee67a643cf
Jose Rodriguez <cheto.rod7@gmail.com>
2017-03-09 21:25:00 -0300

    exploring enron email data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,23 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+count = 0
+countNan = 0
+countPoi = 0
+countPoiNan = 0
 
+for k in enron_data:
+	count += 1
+	if enron_data[k]['poi'] == True:
+		countPoi += 1
+		if enron_data[k]['total_payments'] == 'NaN':
+			countPoiNan += 1
+	
+	if enron_data[k]['total_payments'] == 'NaN':
+		countNan += 1
+		
+		
+print count
+print countNan
+print countPoi
+print countPoiNan
